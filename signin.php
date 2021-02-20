396b5a280adb3796b55eae382001bc93798665d4
Windy Feng <1745500559@qq.com>
2021-02-20 20:53:15 +0800

    Add Logo on Register/Login Functions & add URL followthrough

--- signin.php
+++ signin.php
@@ -30,7 +30,7 @@ use HackSC\UserSystem;
 		if(empty($rEmail) && empty($rPassword)){
 	  ?>
 	  <form action="" method="post">
-	    <img class="mb-4" src="" alt="" width="72" height="57">
+	    <img class="mb-4" src="./img/lipstickLogo.png" alt="" width="72" height="57">
 	    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 	    <label for="inputEmail" class="visually-hidden">Username</label>
 	    <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
@@ -43,7 +43,7 @@ use HackSC\UserSystem;
 	    </div>
 	    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
 	    <p class="text-link">
-			<a href="signup.html">Doesn't have an account?</a>
+			<a href="signup.php<?php if(isset($_GET['URL'])){ echo "?URL=" . $_GET['URL']; } ?>">Doesn't have an account?</a>
 		</p>
 	    <p class="mt-5 mb-3 text-muted">&copy; 2021.2.19</p>
 	  </form>
