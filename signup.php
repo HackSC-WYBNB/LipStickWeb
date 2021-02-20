10a808412fdd5a4aeb92ecda414a3b5ece870ca0
Yibo Wen <yibowen@usc.edu>
2021-02-20 18:07:54 +0800

    Updated home.html

--- signup.php
+++ signup.php
@@ -11,7 +11,7 @@ use HackSC\UserSystem;
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link href="./signin.css" rel="stylesheet">
+    <link href="./css/signin.css" rel="stylesheet">
 
     <title>还没Wuhoooo呢~</title>
   </head>
