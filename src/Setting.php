b182b7a4f0e5118da92183d25afedfb1b5ac74ae
Windy Feng <1745500559@qq.com>
2021-02-21 19:59:20 +0800

    Heroku Deployment

--- src/Setting.php
+++ src/Setting.php
@@ -1,12 +1,12 @@
 <?php
 namespace HackSC;
 class Setting{
-    const DBName = 'hacksc';
-    const HOST = '127.0.0.1';
+    const DBName = empty(getenv('DATABASE_NAME')) ? 'hacksc' : getenv('DATABASE_NAME');
+    const HOST = empty(getenv('DATABSE_HOST')) ? '127.0.0.1' : getenv('DATABASE_HOST');
     const PORT = 3306;
-    const USERNAME = 'hacksc';
-    const PASSWORD = '123456';
+    const USERNAME = empty(getenv('DATABSE_USER')) ? 'hacksc' : getenv('DATABASE_USER');
+    const PASSWORD = empty(getenv('DATABASE_PWD')) ? '123456' : getenv('DATABASE_PWD');
 
-    const DOMAIN = 'localhost';
-    const TOKEN_DOMAIN = '.localhost';
+    const DOMAIN = empty(getenv('DOMAIN')) ? 'localhost' : getenv('DOMAIN');
+    const TOKEN_DOMAIN = '.' . self::DOMAIN;
 }
