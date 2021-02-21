b2a7227a6fcd37ab5f0138c43bc8a7ef337c7acc
Windy Feng <1745500559@qq.com>
2021-02-21 20:25:22 +0800

    Change

--- src/GlobalConn.php
+++ src/GlobalConn.php
@@ -1,6 +1,7 @@
 <?php
 namespace HackSC;
 
+use mysqli;
 use MysqliDb;
 
 class GlobalConn{
@@ -12,9 +13,30 @@ class GlobalConn{
         if(self::$database == null){
             self::connect();
         }
-        if(!file_exists(__DIR__ . '/../install.lock')){
+        if(!file_exists(__DIR__ . '/../install.lock') && False){
             file_put_contents(__DIR__ . '/../install.lock','1');
-            self::$database->mysqli()->query(file_get_contents(__DIR__ . '/../scripts/UserSystem.sql'));
+            self::$database->mysqli()->query(
+                'CREATE TABLE IF NOT EXISTS `users` (
+                `email` VARCHAR(50) NOT NULL,
+                `password` CHAR(64) NOT NULL,
+                PRIMARY KEY ( `email` )
+                )ENGINE=InnoDB CHARSET=utf8;'
+            );
+            self::$database->mysqli()->query(
+                'CREATE TABLE IF NOT EXISTS `tokens` (
+                    `email` VARCHAR(50) NOT NULL,
+                    `token_str` CHAR(32) NOT NULL,
+                    `issued` INT UNSIGNED NOT NULL,
+                    `expires` INT UNSIGNED NOT NULL,
+                    PRIMARY KEY ( `token_str` )
+                )ENGINE=InnoDB CHARSET=utf8;'
+            );
+            self::$database->mysqli()->query(
+                'CREATE TABLE IF NOT EXISTS `photos` (
+                    `email` VARCHAR(50) NOT NULL,
+                    `photo` BLOB NOT NULL
+                );'
+            );
         }
     }
 }
