7863f8295861ee4a7dad454f493d2f314482f113
Windy Feng <1745500559@qq.com>
2021-02-21 20:15:07 +0800

    Update Setting.php

--- src/Setting.php
+++ src/Setting.php
@@ -12,7 +12,7 @@ class Setting{
 }
 Setting::$DBName = empty(getenv('DATABASE_NAME')) ? 'hacksc' : getenv('DATABASE_NAME');
 Setting::$HOST = empty(getenv('DATABSE_HOST')) ? '127.0.0.1' : getenv('DATABASE_HOST');
-Setting::$PORT = empty(getenv('DATABSE_USER')) ? 'hacksc' : getenv('DATABASE_USER');
+Setting::$USERNAME = empty(getenv('DATABSE_USER')) ? 'hacksc' : getenv('DATABASE_USER');
 Setting::$PASSWORD = empty(getenv('DATABASE_PWD')) ? '123456' : getenv('DATABASE_PWD');
 Setting::$DOMAIN = empty(getenv('DOMAIN')) ? 'localhost' : getenv('DOMAIN');
 Setting::$TOKEN_DOMAIN = '.' . Setting::$DOMAIN;
